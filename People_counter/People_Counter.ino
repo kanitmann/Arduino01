604d24172c2d06876caad614807260e4dfab33e1
Kanit Mann <51778919+kanitmann@users.noreply.github.com>
2020-02-05 19:01:01 +0530

    Final code

--- People_counter/People_Counter.ino
+++ People_counter/People_Counter.ino
@@ -11,20 +11,24 @@ void setup() {
   analogWrite(led,0);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
   Action:
-  if(digitalRead(j)==0){
+  if(digitalRead(j)==0)
+  {
     int c=0;
-    while(digitalRead(i)==0){
+    while(digitalRead(i)==0)
+    {
       c++;
       if(c>1000) goto Action;
     }
     count++;
   }
-  if(digitalRead(i)==0){
+  if(digitalRead(i)==0)
+  {
     int c=0;
-    while(digitalRead(j)==0){
+    while(digitalRead(j)==0)
+    {
       c++;
       if(c>1000) goto Action;
     }
@@ -32,13 +36,16 @@ void loop() {
     count--;
    
   }
-  if(count>5){
+  if(count>5)
+  {
     Serial.println("Room Full");
   }
-  else if(count<0){
+  else if(count<0)
+  {
     count=0;
   }
-  else{
+  else
+  {
   analogWrite(led,count*51);
   }
    Serial.println(count);
