d783037c90c337c2d3af9de240b53175da7aa1df
Kanit Mann <51778919+kanitmann@users.noreply.github.com>
2020-02-06 20:54:36 +0530

    Update People_Counter.ino

--- People_counter/People_Counter.ino
+++ People_counter/People_Counter.ino
@@ -1,7 +1,7 @@
 int count = 0;
 int led = 11;
 int i = 2, j = 3;
-
+int n,m;
 void setup() {
   // put your setup code here, to run once:
   pinMode(i,INPUT);
@@ -13,41 +13,24 @@ void setup() {
 
 void loop() 
 {
-  Action:
-  if(digitalRead(j)==0)
+  if(m == digitalRead(i))
   {
-    int c=0;
-    while(digitalRead(i)==0)
+    delay(500);
+    if(n == digitalRead(j))
     {
-      c++;
-      if(c>1000) goto Action;
-    }
-    count++;
-  }
-  if(digitalRead(i)==0)
+      count++;  
+    }  
+  } 
+  if(n == digitalRead(j))
   {
-    int c=0;
-    while(digitalRead(j)==0)
+    delay(500);
+    if(m == digitalRead(i))
     {
-      c++;
-      if(c>1000) goto Action;
-    }
-    Serial.println("DOWN");
-    count--;
-   
+      count--;  
+    }  
   }
-  if(count>5)
-  {
-    Serial.println("Room Full");
-  }
-  else if(count<0)
-  {
-    count=0;
-  }
-  else
-  {
-  analogWrite(led,count*51);
-  }
-   Serial.println(count);
-   delay(500);
+  Serial.println(count);
+  analogWrite(led,51*count);
 }
+ 
+ 
